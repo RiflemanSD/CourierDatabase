a56c2cd977356ede8cf2c50e7bb2abe2c3e051f5
RiflemanSD <sotirisdoudis@windowslive.com>
2016-04-22 22:19:12 +0300

    Small changes to GUI...

--- src/org/riflemansd/courierdb/gui/test/GUIDataTest.java
+++ src/org/riflemansd/courierdb/gui/test/GUIDataTest.java
@@ -44,19 +44,9 @@
 package org.riflemansd.courierdb.gui.test;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.File;
 import java.util.Date;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import org.riflemansd.jxsortabletable.JXSortableTable;
 
 
